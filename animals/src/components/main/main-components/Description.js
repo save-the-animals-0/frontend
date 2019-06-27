007580824b448e20ac96718e525f22f4d5683fec
unknown <robertrey1234@gmail.com>
2019-06-27 18:04:33 -0500

    remade app. Set up file structure with basic JSX. Need to figure out how to pass data from backend in as props.

---
+++ animals/src/components/main/main-components/Description.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Description extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Donation amount of donation goal</h2>
+                <h3>Deadline</h3>
+                <h3>Category: <span>""</span></h3>
+                <p>Description</p>
+            </div>
+        )
+    }
+}
+
+export default Description
